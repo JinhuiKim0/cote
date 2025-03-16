9ed9cbb9f2643042c297d470880a81bd05b0bda4
Kim, Jinhue <ghty68752@naver.com>
2025-03-16 17:13:38 +0900

    B0J11478

--- B0J11478/src/B0J11478/Main.java
+++ B0J11478/src/B0J11478/Main.java
@@ -4,24 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		var reader = new BufferedReader(new InputStreamReader(System.in));
 		String input = reader.readLine();
 		Set<String> set = new HashSet<>();
-		for (int j = 0; j < input.length() - 1; j++) {
-			for (int i = j+1; i < input.length(); i++) {
+		for (int j = 0; j < input.length()+1; j++) {
+			for (int i = j+1; i < input.length()+1; i++) {
 				set.add(input.substring(j, i));
 			}
 		}
-		Iterator iterSet=set.iterator();
-		while(iterSet.hasNext()){
-			System.out.print(iterSet.next()+" ");
-		}
-		System.out.println();
-		System.out.println(set.toString());
 		System.out.print(set.size());
 	}
 }
