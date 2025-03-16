9de51cbbe1e3e4bdf7ae7ce751f25a990c0a6861
Kim, Jinhue <ghty68752@naver.com>
2025-03-16 16:55:35 +0900

    B0J11478 set내역 출력

--- B0J11478/src/B0J11478/Main.java
+++ B0J11478/src/B0J11478/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -11,10 +12,16 @@ public class Main {
 		String input = reader.readLine();
 		Set<String> set = new HashSet<>();
 		for (int j = 0; j < input.length() - 1; j++) {
-			for (int i = j; i < input.length(); i++) {
+			for (int i = j+1; i < input.length(); i++) {
 				set.add(input.substring(j, i));
 			}
 		}
+		Iterator iterSet=set.iterator();
+		while(iterSet.hasNext()){
+			System.out.print(iterSet.next()+" ");
+		}
+		System.out.println();
+		System.out.println(set.toString());
 		System.out.print(set.size());
 	}
 }
