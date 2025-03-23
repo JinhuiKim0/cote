df40de4c2ec706787acb70e47bb72f11bb491afb
Kim, Jinhue <ghty68752@naver.com>
2025-03-23 22:47:22 +0900

    중간 수정

--- H_Index/src/programmers/Solution.java
+++ H_Index/src/programmers/Solution.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        Arrays.sort(citations); // 오름차순 정렬
         int n=citations.length;
-        int[] result= new int[n];
-        for(int i=0;i<n;i++) {
-        	result[i]=Math.min(citations[i], i+1);
+        
+        for(int i=0;i<n;i++) {// 내림차순으로 정렬하지 않고 반대로 루프
+        	//System.out.print(citations[i]);
+        	int remain=i+1;// 현재 남은 논문의 개수
+        	if(remain<=citations[i]) {
+        		answer=remain;
+        	}
         }
-        Arrays.sort(result); // 오름차순 정렬
-        answer=result[n-1]; // 제일 마지막 인덱스 배열의 값
         return answer;
     }
 }
