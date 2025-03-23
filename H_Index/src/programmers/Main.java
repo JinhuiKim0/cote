3b8bfdd8280e30dac52988ab7bff130b4c68b45c
Kim, Jinhue <ghty68752@naver.com>
2025-03-23 21:22:06 +0900

    main클래스 수정1

--- H_Index/src/programmers/Main.java
+++ H_Index/src/programmers/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		String[] str=input.split(",");
 		int[] citations=new int[str.length];
 		for(int i=0;i<str.length;i++) {
-			citations[i]=Integer.parseInt(str[i]); // String ->Integer 형변환
+			citations[i]=Integer.parseInt(str[i].trim()); // String ->Integer 형변환
 		}
 		Solution solution=new Solution();
 		System.out.print(solution.solution(citations));
