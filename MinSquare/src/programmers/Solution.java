f2294bb51896a0994a9ae6e7e1f039f80644a8a0
Kim, Jinhue <ghty68752@naver.com>
2025-03-24 20:49:54 +0900

    변수 이름 수정, Solution해결

--- MinSquare/src/programmers/Solution.java
+++ MinSquare/src/programmers/Solution.java
@@ -6,20 +6,23 @@ package programmers;
 //세로의 길이가 가로의 길이보다 커야함 작다면 세로의 길이와 가로의 길이의 위치를 바꿔줌
 //다 정렬하고 나면 가로의 길이들을 비교 가장 큰 값 저장, 세로도 마찬가지
 //return 값은 최대의 가로세로길이를 곱한 값
+// - 매개변수 이름을 잘못 알고 사용
 class Solution {
  public int solution(int[][] sizes) {
      int answer = 0;
      int max=0;
      int min=0;
-     for(int i=0;i<size.length;i++){
-         max=Math.max(size[i][0],size[i][1]);
-         min=Math.min(size[i][0],size[i][1]);
-         size[i][0]=max;
-         size[i][1]=min;
+     for(int i=0;i<sizes.length;i++){
+         max=Math.max(sizes[i][0],sizes[i][1]);
+         min=Math.min(sizes[i][0],sizes[i][1]);
+         sizes[i][0]=max;
+         sizes[i][1]=min;
      }
-     for(int i=0;i<size.length;i++){
-         max=Math.max(size[i][0],max); 
-         min=Math.max(size[i][1],min);
+     max=0;
+     min=0;
+     for(int i=0;i<sizes.length;i++){
+         max=Math.max(sizes[i][0],max); 
+         min=Math.max(sizes[i][1],min);
      }
      answer=max*min;
      return answer;
