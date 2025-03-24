b0ce7e5bf8d8184fa0b1f867befb152bb2e5b5b8
Kim, Jinhue <ghty68752@naver.com>
2025-03-24 20:36:33 +0900

    Solution해결

--- IntToString/src/programmers/Solution.java
+++ IntToString/src/programmers/Solution.java
@@ -9,11 +9,7 @@ class Solution {
  public int solution(String s) {
      int answer = 0;
      
-     if(s.charAt(0)=="-"){
-         answer=0-Integer.parseInt(s.substring(1,s.length()));
-     }else{
-         answer=Integer.parseInt(s.substring(1,s.length()));
-     }
+     answer=Integer.parseInt(s);
      return answer;
      
  }
