b8a95deecc718851058a2fd12820e5c02fb0fb43
Kim, Jinhue <ghty68752@naver.com>
2025-03-24 20:35:49 +0900

    IntToString 첫시도

---
+++ IntToString/src/programmers/Solution.java
@@ -0,0 +1,20 @@
+package programmers;
+
+//문자열을 숫자로
+//1<=s.length()<=5
+//맨앞 부호 올수 있음
+//0으로 시작X
+//부화와 숫자로 이루어져 있음
+class Solution {
+ public int solution(String s) {
+     int answer = 0;
+     String c = s.charAt(0);
+     if(c.equal("-")){
+         answer=0-Integer.parseInt(s.substring(1,s.length()));
+     }else{
+         answer=Integer.parseInt(s.substring(1,s.length()));
+     }
+     return answer;
+     
+ }
+}
