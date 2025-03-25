cac815f81faf01cac2033c3fa6ada595ca37700f
Kim, Jinhue <ghty68752@naver.com>
2025-03-26 00:38:25 +0900

    solution해결

--- SumOfNumberOfMeasure/src/programmers/Solution.java
+++ SumOfNumberOfMeasure/src/programmers/Solution.java
@@ -5,19 +5,20 @@ class Solution {
         int answer = 0;
         for(int i=left;i<=right;i++) {
         	int number=0;
-        	for(int j=1;j<=Math.sqrt(i);j++) {
-        		if(i%j==0) { // 약수
-        			number++;
-        		}
-        	}
-        	number=number*2;
-        	if(Math.sqrt(i)*Math.sqrt(i)==i) {
-        		number--;
-        	}
+        	for (int j = 1; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) { // j가 i의 약수인 경우
+                    number++; // 약수 개수 증가
+                    
+                    // j와 i/j가 다르면 서로 다른 약수이므로 개수 2개 증가
+                    if (j != i / j) {
+                        number++;
+                    }
+                }
+            }
         	if(number%2==0) {
-        		answer+=number;
+        		answer+=i;
         	}else {
-        		answer-=number;
+        		answer-=i;
         	}
         }
         return answer;
