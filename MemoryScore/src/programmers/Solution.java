2ac45b83242f062841a836499e9407aaa7aa35bf
Kim, Jinhue <ghty68752@naver.com>
2025-03-26 00:13:30 +0900

    추억점수 문제 해결

--- MemoryScore/src/programmers/Solution.java
+++ MemoryScore/src/programmers/Solution.java
@@ -3,21 +3,26 @@ package programmers;
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution {
+public class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer =new int[photo.length];
-        Map<String, Integer> map=new HashMap<>();
-        for(int i=0;i<photo.length;i++) {
-        	for(int j=0;j<name.length;j++) {
-        		map.put(name[j], 0);
-        	}
-        	for(int j=0;j<photo[i].length;j++) {
-        		map.put(photo[i][j], map.getOrDefault(photo[i][j],0)+1);
-        	}
-        	for(int j=0;j<photo[i].length;j++) {
-        		answer[i]+=map.get(name[j])*yearning[j];
-        	}
+        // 이름과 점수를 매핑할 HashMap 생성
+        Map<String, Integer> scoreMap = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            scoreMap.put(name[i], yearning[i]);
         }
+
+        // 결과를 담을 배열
+        int[] answer = new int[photo.length];
+        
+        // 각 사진에 대해 점수 합산
+        for (int i = 0; i < photo.length; i++) {
+            int totalScore = 0;
+            for (String person : photo[i]) {
+                totalScore += scoreMap.getOrDefault(person, 0); // person에 해당하는 점수 합산, 없으면 0
+            }
+            answer[i] = totalScore; // 해당 사진의 점수 기록
+        }
+        
         return answer;
     }
 }
