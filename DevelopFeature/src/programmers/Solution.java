53ee3788bed12250dc772073dc49d22ba03ec65d
Kim, Jinhue <ghty68752@naver.com>
2025-03-28 03:52:53 +0900

    오류 수정

--- DevelopFeature/src/programmers/Solution.java
+++ DevelopFeature/src/programmers/Solution.java
@@ -6,8 +6,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
-    public List solution(int[] progresses, int[] speeds) {
-        List<Integer> answer =new LinkedList<>();
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answerList =new LinkedList<>();
         Deque<Integer> queue=new ArrayDeque<>(); // 개발 날짜
         int num=0;
         for(int i=0;i<progresses.length;i++) { // 각자 개발 날짜 입력
@@ -18,17 +18,22 @@ class Solution {
         		queue.addLast((num/speeds[i])+1);
         	}
         }
-        int Max=queue.remove(); // 일단 제일 처음 값을 제일 큰 값으로 넣기
+        int Max=queue.removeFirst(); // 일단 제일 처음 값을 제일 큰 값으로 넣기
         for(int i=1;i<queue.size();i++) {
         	int count=1; // 기본적으로 본인을 포함해야하기때문에
-        	int number=queue.remove(); // 다음값
+        	int number=queue.removeFirst(); // 다음값
         	if(Max>=number) { // Max보다 작거나 같은 경우
         		count++;
         	}else { // Max보다 큰 경우
         		Max=number;
-        		answer.add(count);
+        		answerList.add(count);
         	}
         }
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+        
         return answer;
     }
 }
