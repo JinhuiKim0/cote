b37c1d42cf35499cabd103535c09252bdeab5db8
Kim, Jinhue <ghty68752@naver.com>
2025-03-28 06:15:37 +0900

    효율성 테스트 실패

--- RightParentheses/src/programmers/Solution.java
+++ RightParentheses/src/programmers/Solution.java
@@ -5,7 +5,6 @@ import java.util.Deque;
 
 class Solution {
 	boolean solution(String s) {
-		boolean answer = true;
 		Deque<String> stack = new ArrayDeque<>(); // 괄호를 넣을 stack
 		String[] str = s.split(""); // 각각 문자열 분리
 		for (int i = 0; i < str.length; i++) {
@@ -16,19 +15,14 @@ class Solution {
 					return false; // 바로 끝낼 수 있도록 함.
 				} else {
 					String sr = stack.pop();
-					if (sr.equals("(")) {
-						break;
-					} else {
+					if (sr.equals("(")) {}
+					else {
 						return false;
 					}
 				}
 
 			}
-			if(stack.isEmpty()) {
-				return true;
-			}else {
-				return false;
-			}
 		}
+			return stack.isEmpty();
 	}
 }
