5c2e87893be9285756f1ea7ef392eb266c0dd0aa
Kim, Jinhue <ghty68752@naver.com>
2025-03-28 06:01:45 +0900

    예외 수정

--- RightParentheses/src/programmers/Solution.java
+++ RightParentheses/src/programmers/Solution.java
@@ -4,22 +4,27 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        Deque<String> stack=new ArrayDeque<>(); // 괄호를 넣을 stack
-        String[] str=s.split(""); // 각각 문자열 분리
-        for(int i=0;i<str.length;i++) {
-        	if(str[i].equals("(")) {
-        		stack.push("(");
-        	}else if(str[i].equals(")")) {
-        		String sr=stack.pop();
-        		if(sr.equals("(")) {
-        			answer=true;
-        		}else {
-        			answer=false;
-        		}
-        	}
-        }
-        return answer;
-    }
+	boolean solution(String s) {
+		boolean answer = true;
+		Deque<String> stack = new ArrayDeque<>(); // 괄호를 넣을 stack
+		String[] str = s.split(""); // 각각 문자열 분리
+		for (int i = 0; i < str.length; i++) {
+			if (str[i].equals("(")) {
+				stack.push("(");
+			} else if (str[i].equals(")")) {
+				if (stack.isEmpty()) {
+					answer = false;
+				} else {
+					String sr = stack.pop();
+					if (sr.equals("(")) {
+						answer = true;
+					} else {
+						answer = false;
+					}
+				}
+
+			}
+		}
+		return answer;
+	}
 }
