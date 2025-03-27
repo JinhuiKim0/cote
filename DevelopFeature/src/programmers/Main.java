c2cd06629b790f8e353bdd9ac8b507ed866f63a6
Kim, Jinhue <ghty68752@naver.com>
2025-03-28 04:26:56 +0900

    solution재구성, main작성

---
+++ DevelopFeature/src/programmers/Main.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		var reader=new BufferedReader(new InputStreamReader(System.in));
+		String[] input=reader.readLine().split(",");
+		int[] progresses=new int[input.length];
+		for(int i=0;i<input.length;i++) {
+			progresses[i]=Integer.parseInt(input[i].trim());
+		}
+		String[] inp=reader.readLine().split(",");
+		int[] speeds=new int[inp.length];
+		for(int i=0;i<inp.length;i++) {
+			speeds[i]=Integer.parseInt(inp[i].trim());
+		}
+		Solution sol=new Solution();
+		int[] result=sol.solution(progresses, speeds);
+		for(int i=0;i<result.length;i++) {
+			System.out.print(result[i]); 
+		}
+	}
+
+}
--- DevelopFeature/src/programmers/Solution.java
+++ DevelopFeature/src/programmers/Solution.java
@@ -1,39 +1,59 @@
 package programmers;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
-class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answerList =new LinkedList<>();
-        Deque<Integer> queue=new ArrayDeque<>(); // 개발 날짜
-        int num=0;
-        for(int i=0;i<progresses.length;i++) { // 각자 개발 날짜 입력
-        	num=100-progresses[i];
-        	if(num%speeds[i]==0) {
-        		queue.addLast(num/speeds[i]);
-        	}else { // 나눴을때 나누어떨어지지 않으면 +1을 해줘야함.
-        		queue.addLast((num/speeds[i])+1);
-        	}
-        }
-        int Max=queue.removeFirst(); // 일단 제일 처음 값을 제일 큰 값으로 넣기
-        for(int i=1;i<queue.size();i++) {
-        	int count=1; // 기본적으로 본인을 포함해야하기때문에
-        	int number=queue.removeFirst(); // 다음값
-        	if(Max>=number) { // Max보다 작거나 같은 경우
-        		count++;
-        	}else { // Max보다 큰 경우
-        		Max=number;
-        		answerList.add(count);
-        	}
-        }
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
-    }
+public class Solution {
+	public int[] solution(int[] progresses,int[] speeds) {
+		// 선입선출 필요
+		Queue<Integer> queue=new LinkedList<>(); // 며칠이 걸리는 지를 저장.
+		int max=0; // 최대값
+		int count=0; // 개수 계산
+		// 며칠 걸리는지 계산
+		for(int i=0;i<progresses.length;i++) {
+			int remains=100-progresses[i]; // 남은 진도.
+			int num=remains/speeds[i]; 
+			if(queue.isEmpty()) { // queue가 비어있다면
+				if(remains%speeds[i]==0) { // 딱 맞아 떨어지면
+				queue.add(num);
+				max=num; // 최대값을 num으로 저장. queue가 비어있는 상태이기 떄문
+				}else { // 딱 맞아떨어지지 않는경우
+					queue.add(num+1);
+					max=num+1;// 최대값을 num+1로 저장. queue가 비어있는 상태이기 떄문
+				}
+			}else { // queue가 비어있지 않은 경우
+				if(remains%speeds[i]==0) { // 딱 맞아 떨어지면
+					if(max>=num) { // 최대값보다 num이 작으면
+						queue.add(max); // max를 대신 넣음.
+					}else { // 최대값보다 큰경우
+						max=num;
+						queue.add(max);
+					}
+				}else { // 딱 맞아떨어지지 않는경우
+					if(max>=num+1) { // 최대값보다 num이 작으면
+						queue.add(max); // max를 대신 넣음.
+					}else { // 최대값보다 큰경우
+						max=num+1;
+						queue.add(max);
+					}
+				}
+			}
+		}
+		// queue내부에 중복되게 작성해놓았으니 갯수 계산
+		// 앞의 Integer은 순서를 얘기해줌. 뒤의 Integer은 GetOfDefault을 1로 하여 기본은 1이고 중복되어나오면 수가 증가
+		Map<Integer,Integer> map=new HashMap<>(); 
+		for(int i=1;i<=queue.size();i++) {
+			map.put(i, map.getOrDefault(i, 0)+1);
+		}
+		int[] answer=new int[map.size()];
+		// map에 있는 내용을 answer배열에 담기
+		for(int i=1;i<=map.size();i++) {
+			answer[i-1]=map.get(i);
+		}
+		return answer;
+	}
 }
+
+
