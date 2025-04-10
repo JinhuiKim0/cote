55e32f8cf33f937f78b2221de5d1cf892b77a8cb
Kim, Jinhue <ghty68752@naver.com>
2025-04-11 04:14:14 +0900

    MoreSpicy실패

---
+++ MoreSpicy/src/programmers/Main.java
@@ -0,0 +1,15 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] scoville= {1, 2, 3, 9, 10, 12};
+		int K=7;
+		Solution s=new Solution();
+		System.out.println(s.solution(scoville, K));
+
+	}
+
+}
---
+++ MoreSpicy/src/programmers/Solution.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i:scoville) {
+        	pq.add(i);
+        }
+        for(int i=0;i<scoville.length-1;i++) {
+        	if(pq.peek()>=K) { // 최소힙이 K와 같으면 중단.
+        		break;
+        	}else { // K보다 작은경우
+        		int first=pq.poll();
+        		int second=pq.poll();
+        		pq.add(first+(second*2));
+        		answer++;
+        	}
+        }
+        if(answer==0) {
+        	answer=-1;
+        }
+        return answer;
+    }
+}
